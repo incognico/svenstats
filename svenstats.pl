44066595bdf588bab3e8f1201bf3e3429d06b9b8
Nico R. Wohlgemuth <nico@lifeisabug.com>
2016-05-17 16:24:11 +0200

    Add map blacklist & gather map stats

--- svenstats.pl
+++ svenstats.pl
@@ -10,6 +10,8 @@ use utf8;
 use strict; 
 use warnings; 
 
+no warnings 'experimental::smartmatch';
+
 use DBI;
 use Data::Dumper;
 use Geo::IP;
@@ -17,14 +19,16 @@ use Math::BigFloat;
 
 ### config
 
-my $db     = "$ENV{'HOME'}/scstats/scstats.db";
-my $geo    = "$ENV{'HOME'}/scstats/GeoLiteCity.dat";
-my $maxinc = 420; # maximum score difference between two datapoints to prevent arbitrary player scores set by some maps
-my $debug  = 0;   # 1 prints debug output and won't use the DB
+my $db        = "$ENV{'HOME'}/scstats/scstats.db";
+my $geo       = "$ENV{'HOME'}/scstats/GeoLiteCity.dat";
+my $maxinc    = 420; # maximum score difference between two datapoints to prevent arbitrary player scores set by some maps
+my $debug     = 0;   # 1 prints debug output and won't use the DB
+my @blacklist = qw(mmm); # map blacklist, space seperated, lowercase
 
 ###
 
-my ($dbh, $stats);
+my $hold = 0;
+my ($dbh, $stats, $maps);
 
 if (@ARGV != 1) {
    say "Usage: $0 <logfile>";
@@ -41,10 +45,7 @@ unless ($debug) {
       exit;
    }
 
-   my $sth = $dbh->prepare('SELECT steamid, name, id, score, lastscore, deaths, lastdeaths, joins, geo, lat, lon, datapoints FROM stats');
-   $sth->execute;
-   $stats = $sth->fetchall_hashref('steamid');
-   $sth->finish;
+   $stats = $dbh->selectall_hashref('SELECT steamid, name, id, score, lastscore, deaths, lastdeaths, joins, geo, lat, lon, datapoints FROM stats', 'steamid');
 
    for (keys %{$stats}) {
       if (defined $$stats{$_}{score}) {
@@ -56,6 +57,11 @@ unless ($debug) {
       $$stats{$_}{lastscore}     = Math::BigFloat->new($$stats{$_}{lastscore}) if(defined $$stats{$_}{lastscore});
       $$stats{$_}{idx}           = $$stats{$_}{id}.'x'.$$stats{$_}{joins}      if(defined $$stats{$_}{id} && defined $$stats{$_}{joins});
    }
+
+   $maps = $dbh->selectall_hashref('SELECT map, count FROM maps', 'map');
+
+   my $res = $dbh->selectrow_hashref('SELECT hold FROM misc WHERE rowid = 1');
+   $hold = $$res{hold} if(defined $$res{hold});
 }
 
 unless (open my $fh, '<', $ARGV[0]) { 
@@ -68,53 +74,66 @@ else {
       my $line = substr($in, 0, 2).substr($in, 25);
       say $line if($debug);
 
-      if ($line =~ /^L "(.+)<([0-9]+)><STEAM_(0:[01]:[0-9]+)><>" connected, address "([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):/) {
-         $$stats{$3}{name}  = $1;
-         $$stats{$3}{id}    = $2;
-         $$stats{$3}{ip}    = $4;
-         $$stats{$3}{joins}++;
-      }
-      elsif ($line =~ /^L ".+<[0-9]+><STEAM_(0:[01]:[0-9]+)><players>" has entered the game/) {
-         $$stats{$1}{joins}++;
-      }
-      elsif ($line =~ /^L "(.+)<([0-9]+)><STEAM_(0:[01]:[0-9]+)><players>" stats: frags="(-?[0-9]+\.[0-9]{2})" deaths="([0-9]+)"/) {
-         $$stats{$3}{score}      = Math::BigFloat->bzero unless(defined $$stats{$3}{score});
-         $$stats{$3}{lastscore}  = Math::BigFloat->bzero unless(defined $$stats{$3}{lastscore});
-         $$stats{$3}{deaths}     = 0 unless(defined $$stats{$3}{deaths});
-         $$stats{$3}{lastdeaths} = 0 unless(defined $$stats{$3}{lastdeaths});
-
-         my $score     = Math::BigFloat->new($4);
-         my $lastscore = $score->copy;
-         my $idx       = $2.'x'.(defined $$stats{$3}{joins} ? $$stats{$3}{joins} : 1);
-
-         if ($score->bacmp($$stats{$3}{lastscore})) {
-            if (exists $$stats{$3}{idx} && $idx eq $$stats{$3}{idx}) {
-               my $diff = $score->bsub($$stats{$3}{lastscore});
-               $$stats{$3}{score}->badd($diff) unless($diff->copy->babs > $maxinc);
-               say "old: $$stats{$3}{lastscore} | diff: $diff | new: $$stats{$3}{score}" if($debug);
-            }
-            else {
-               $$stats{$3}{score}->badd($score) unless($score->copy->babs > $maxinc);
-               say "old: $$stats{$3}{lastscore} | diff: $score | new: $$stats{$3}{score}" if($debug);
-            }
+      if ($line =~ /^L Started map "(.+)" \(CRC "-?[0-9]+"\)/) {
+         if (lc($1) ~~ @blacklist) {
+            $hold = 1;
+         }
+         else {
+            $hold = 0;
          }
 
-         if ($5 != $$stats{$3}{lastdeaths}) {
-            if (exists $$stats{$3}{idx} && $idx eq $$stats{$3}{idx}) {
-               my $diff = $5 - $$stats{$3}{lastdeaths};
-               $$stats{$3}{deaths} += $diff;
-            }
-            else {
-               $$stats{$3}{deaths} += $5;
-            }
-         }
+         $$maps{lc($1)}{count}++;
+      }
 
-         $$stats{$3}{name}       = $1;
-         $$stats{$3}{id}         = $2;
-         $$stats{$3}{idx}        = $idx;
-         $$stats{$3}{lastscore}  = $lastscore->copy;
-         $$stats{$3}{lastdeaths} = $5;
-         $$stats{$3}{datapoints}++;
+      unless ($hold) {
+         if ($line =~ /^L "(.+)<([0-9]+)><STEAM_(0:[01]:[0-9]+)><>" connected, address "([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):/) {
+            $$stats{$3}{name}  = $1;
+            $$stats{$3}{id}    = $2;
+            $$stats{$3}{ip}    = $4;
+            $$stats{$3}{joins}++;
+         }
+         elsif ($line =~ /^L ".+<[0-9]+><STEAM_(0:[01]:[0-9]+)><players>" has entered the game/) {
+            $$stats{$1}{joins}++;
+         }
+         elsif ($line =~ /^L "(.+)<([0-9]+)><STEAM_(0:[01]:[0-9]+)><players>" stats: frags="(-?[0-9]+\.[0-9]{2})" deaths="([0-9]+)"/) {
+            $$stats{$3}{score}      = Math::BigFloat->bzero unless(defined $$stats{$3}{score});
+            $$stats{$3}{lastscore}  = Math::BigFloat->bzero unless(defined $$stats{$3}{lastscore});
+            $$stats{$3}{deaths}     = 0 unless(defined $$stats{$3}{deaths});
+            $$stats{$3}{lastdeaths} = 0 unless(defined $$stats{$3}{lastdeaths});
+
+            my $score     = Math::BigFloat->new($4);
+            my $lastscore = $score->copy;
+            my $idx       = $2.'x'.(defined $$stats{$3}{joins} ? $$stats{$3}{joins} : 1);
+
+            if ($score->bacmp($$stats{$3}{lastscore})) {
+               if (exists $$stats{$3}{idx} && $idx eq $$stats{$3}{idx}) {
+                  my $diff = $score->bsub($$stats{$3}{lastscore});
+                  $$stats{$3}{score}->badd($diff) unless($diff->copy->babs > $maxinc);
+                  say "old: $$stats{$3}{lastscore} | diff: $diff | new: $$stats{$3}{score}" if($debug);
+               }
+               else {
+                  $$stats{$3}{score}->badd($score) unless($score->copy->babs > $maxinc);
+                  say "old: $$stats{$3}{lastscore} | diff: $score | new: $$stats{$3}{score}" if($debug);
+               }
+            }
+
+            if ($5 != $$stats{$3}{lastdeaths}) {
+               if (exists $$stats{$3}{idx} && $idx eq $$stats{$3}{idx}) {
+                  my $diff = $5 - $$stats{$3}{lastdeaths};
+                  $$stats{$3}{deaths} += $diff;
+               }
+               else {
+                  $$stats{$3}{deaths} += $5;
+               }
+            }
+
+            $$stats{$3}{name}       = $1;
+            $$stats{$3}{id}         = $2;
+            $$stats{$3}{idx}        = $idx;
+            $$stats{$3}{lastscore}  = $lastscore->copy;
+            $$stats{$3}{lastdeaths} = $5;
+            $$stats{$3}{datapoints}++;
+         }
       }
    }
    close $fh; 
@@ -156,8 +175,19 @@ unless ($debug) {
          defined $$stats{$_}{olddatapoints} ? $$stats{$_}{datapoints}-$$stats{$_}{olddatapoints}    : 0,
       );
    }
-
    $dbh->commit;
+
+   $sth = $dbh->prepare('REPLACE INTO maps (map, count) VALUES (?,?)');
+
+   for (keys %{$maps}) {
+      $sth->execute($_, $$maps{$_}{count});
+   }
+   $dbh->commit;
+
+   $sth = $dbh->prepare('REPLACE INTO misc (rowid, hold) VALUES (1,?)');
+   $sth->execute($hold);
+   $dbh->commit;
+
    $dbh->disconnect;
 }
 else {
