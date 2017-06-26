70eea760d93e7d945d67a60b84185e6e3c96b458
nico <nico@lifeisabug.com>
2017-06-26 17:24:29 +0200

    Compress old logs and also remove them from SvenDS

--- svenstats_cronjob.bash
+++ svenstats_cronjob.bash
@@ -11,8 +11,17 @@
 
 yesterday=$(date --date='1 day ago' "+%Y-%m-%d")
 svenstats="/home/srcds/scstats/svenstats.pl"
+#svenstatsdb="/home/srcds/scstats/scstats.db"
 svenlogpath="/home/srcds/sc5/svencoop/logs"
 statslogpath="/home/srcds/scstats/logs"
+#twlzstatsfile="/home/srcds/sc5/svencoop_addon/scripts/plugins/twlzstats.txt"
 
 cp "${svenlogpath}/${yesterday}.log" "${statslogpath}/${yesterday}.log"
 $svenstats "${statslogpath}/${yesterday}.log"
+gzip "${statslogpath}/${yesterday}.log"
+
+if [ -s "${statslogpath}/${yesterday}.log.gz" ]; then
+  rm "${statslogpath}/${yesterday}.log"
+fi
+
+#sqlite3 -list -separator ' ' $twlzstatsfile 'select steamid,cast(score + 0.5 as int),deaths from stats where score >1000 order by score desc' | cat -n | sed -e "s/[[:space:]]\+/ /g" | cut -c 2- > $twlzstatsfile
