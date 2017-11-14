b9d0d2a1b4f6147f95df94d9372af79da5ffbd4f
nico <nico@lifeisabug.com>
2017-11-14 23:05:07 +0100

    Update svenstats_cronjob.bash

--- svenstats_cronjob.bash
+++ svenstats_cronjob.bash
@@ -11,17 +11,21 @@
 
 yesterday=$(date --date='1 day ago' "+%Y-%m-%d")
 svenstats="/home/srcds/scstats/svenstats.pl"
-#svenstatsdb="/home/srcds/scstats/scstats.db"
+svenstatsdb="/home/srcds/scstats/scstats.db"
 svenlogpath="/home/srcds/sc5/svencoop/logs"
 statslogpath="/home/srcds/scstats/logs"
 #twlzstatsfile="/home/srcds/sc5/svencoop_addon/scripts/plugins/twlzstats.txt"
+#hldschat="/home/srcds/scstats/hldschat.pl"
+#chatlogpath="/home/srcds/sc5/svencoop_addon/chatlogs"
 
 cp "${svenlogpath}/${yesterday}.log" "${statslogpath}/${yesterday}.log"
 $svenstats "${statslogpath}/${yesterday}.log"
+#$hldschat "${statslogpath}/${yesterday}.log" >> "${chatlogpath}/${yesterday}.html"
+
 gzip "${statslogpath}/${yesterday}.log"
 
-if [ -s "${statslogpath}/${yesterday}.log.gz" ]; then
+if [ -f "${statslogpath}/${yesterday}.log.gz" ]; then
   rm "${statslogpath}/${yesterday}.log"
 fi
 
-#sqlite3 -list -separator ' ' $twlzstatsfile 'select steamid,cast(score + 0.5 as int),deaths from stats where score >1000 order by score desc' | cat -n | sed -e "s/[[:space:]]\+/ /g" | cut -c 2- > $twlzstatsfile
+#sqlite3 -list -separator ' ' $svenstatsdb 'select steamid,cast(score + 0.5 as int),deaths from stats where score >2500 order by score desc' | cat -n | sed -e "s/[[:space:]]\+/ /g" | cut -c 2- > $twlzstatsfile
